de63821f970ffe34e2e72a959901982d6b04f665
STEVEN LY <steven.ly@nomachine.univ-eiffel.fr>
2024-09-18 18:03:13 +0200

    TP1 1.7

--- TP1/src/main/scala/fr/uge/td/scala/Bike.scala
+++ TP1/src/main/scala/fr/uge/td/scala/Bike.scala
@@ -1,28 +1,49 @@
 package fr.uge.td.scala
 
-class Bike (var color:String,var wheelSize:Int,var speed:Int=0){
+class Bike (val color:String,val wheelSize:Int,val speed:Int=0){
 
-  def speedUp(value:Int): Unit = {
-    if(value < 0) throw new IllegalArgumentException("negative speed");
-    speed+=value
+  def speedUp(value:Int): Either[String,Bike] = {
+    if(value < 0) Left("negative value")
+    else Right(new Bike(color, wheelSize, speed+value))
   };
-  def break(value:Int): Unit = {
-    if(value < 0) throw new IllegalArgumentException("negative speed");
-    speed-=value
+  def break(value:Int): Either[String,Bike] = {
+    if(value < 0) Left("negative value")
+    else Right(new Bike(color, wheelSize, speed-value))
   };
-
-
 }
 
 object Bike{
+
+
   def main(args:Array[String]):Unit={
     val bike = new Bike("red",20);
-    println(s"Bike color is  -> ${bike.color}");
-    bike.speedUp(10);
-    println(s"Bike speed is -> ${bike.speed}")
-    bike.break(2);
-//    println(s"Bike speed is -> ${bike.speed}")
-//    bike.break(-2);
-
+    println(s"Bike color is  -> ${bike.color}")
+    println(s"Bike speed is ${bike.speed}")
+    val bike2 = bike.speedUp(10);
+    bike2 match{
+      case Left("negative value")=> println("Error");
+      case Right(value)=> println(s"Bike speed is ${value.speed}")
+        val bike3 = value.speedUp(-1);
+      bike3 match {
+        case Left("negative value")=> println("Error");
+        case Right(value) => println(s"Bike speed is ${value.speed}");
+          val bike4 = value.break(5);
+      }
+    }
+    println("new bike")
+    bike2 match{
+      case Left("negative value")=> println("Error");
+      case Right(value)=> println(s"Bike speed is ${value.speed}")
+        val bike3 = value.speedUp(10);
+        bike3 match {
+          case Left("negative value")=> println("Error");
+          case Right(value) => println(s"Bike speed is ${value.speed}");
+            val bike4 = value.break(5);
+            bike4 match {
+              case Left("negative value")=> println("Error");
+              case Right(value) => println(s"Bike speed is ${value.speed}");
+            }
+        }
+    }
   }
 }
