382638bde02d069dc291f3749b09f86c6e4cd1cb
LY-IENG Steven <steven.ly412@gmail.com>
2024-10-05 01:29:26 +0200

    tp3 finished

--- TP3/src/fr/uge/td/scala/HashTable.scala
+++ TP3/src/fr/uge/td/scala/HashTable.scala
@@ -2,50 +2,47 @@ package fr.uge.td.scala
 import java.io.RandomAccessFile
 class HashTable(val fileBdd :String,val offset : Int,val threshold: Double){
   var hashMap = Map[String,Int]()
+  private val fileReader = new RandomAccessFile(fileBdd,"rw")
+  var addIndex= 0
 
 
-  def addToMap()={
-    val fileReader = new RandomAccessFile(fileBdd,"rw");
-    val readed = fileReader.readUTF().split("",offset)
-    var current:Int=0
-    readed.foreach(e=> {
-      hashMap = hashMap + (e.split(":")(0) -> current)
-      current+=offset
-    })
+  def closeFile()={
     fileReader.close()
   }
 
-  def overrideFile(): Unit = {
-    val newFile = "new"+fileBdd
-    val fileWriter = new RandomAccessFile(newFile,"rw")
-    addToMap()
-    hashMap.foreach(k => {
-      val keyValue: String = k._1 + ":"+get(k._1).toString
-      val nbStar = offset -keyValue.length
-      keyValue:++"*"*nbStar
-      fileWriter.writeUTF(keyValue)
+  def overrideFile()={
+    var str :String = ""
+    var buffer = new Array[Byte](offset)
+    addIndex=0
+    hashMap.foreach(e=> {
+      fileReader.seek(e._2)
+      fileReader.read(buffer)
+      str = str++:new String(buffer)
+      hashMap = hashMap + (e._1-> addIndex)
+      addIndex+= offset
     })
-    fileWriter.close()
+    val resBuffer = str.getBytes
+    fileReader.setLength(0)
+    fileReader.write(resBuffer,0,resBuffer.length)
   }
 
   def get(key : String) : Option[Int] ={
     val value = hashMap.get(key)
-    val find = new RandomAccessFile(fileBdd,"rw")
-    find.seek(value.get)
-    val buffer = new Array[Byte](offset)
-    find.read(buffer)
-    find.close()
-    Option(new String(buffer).replace("*","").toInt)
+    if(value.isDefined){
+      val buffer = new Array[Byte](offset)
+      fileReader.seek(value.get)
+      fileReader.read(buffer)
+      Some(new String(buffer).replaceAll("\\*", "").split(":")(1).toInt)
+    }
+    else None
   }
 
   def checkThresHold(): Unit = {
-    val fileReader = new RandomAccessFile(fileBdd,"rw");
-    if(fileReader.length()>0){
-      val readed = fileReader.readUTF().split("",offset)
-      if(hashMap.nonEmpty){
-        if(readed.length / hashMap.size >threshold){
-          overrideFile()
-        }
+    if(hashMap.nonEmpty) {
+      println(s"${(hashMap.size.toDouble*offset / addIndex.toDouble) }")
+      if((hashMap.size.toDouble*offset / addIndex.toDouble) < threshold){
+        println("hey")
+        overrideFile()
       }
     }
 
@@ -53,11 +50,14 @@ class HashTable(val fileBdd :String,val offset : Int,val threshold: Double){
 
   def add(key:String, value:Int): Unit={
     checkThresHold()
-      val keyValue : String = key+":"+ value
-      val nbStar = offset - keyValue.length
-      keyValue:++"*"*nbStar
-      val file = new RandomAccessFile(fileBdd,"rw")
-      file.writeUTF(keyValue)
+    val keyValue : String = key+":"+ value
+    val nbStar = offset - keyValue.length
+    val res = keyValue:++"*"*nbStar
+    hashMap = hashMap + (key-> addIndex)
+    addIndex+=offset
+    fileReader.seek(fileReader.length)
+    fileReader.write(res.getBytes())
+
   }
 
 
@@ -70,6 +70,21 @@ object HashTable{
   def main(args: Array[String]): Unit = {
     val hash: HashTable = new HashTable("txt.txt",20,0.4)
     hash.add("abc",777)
-    hash.add("bcd",546)
+    hash.add("def",541)
+    hash.add("def",542)
+    hash.add("def",543)
+    hash.add("def",544)
+    hash.add("def",545)
+    hash.add("def",546)
+    hash.add("abc",771)
+    hash.add("abc",772)
+    hash.add("abc",773)
+    hash.add("abc",774)
+
+
+    val res = hash.get("def")
+    println(s"${res}")
+    println(s"${hash.hashMap}")
+    hash.closeFile()
   }
 }
